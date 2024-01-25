942469976130a9a4c69a7889a0b178f4ec84a92e
Pedro Martins Barbosa Campos <engpedrocampos@gmail.com>
2024-01-25 11:10:41 -0300

    feat: add applying for jobs controller

---
+++ app/Http/Controllers/JobApplicationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobApplicationController extends Controller
+{
+    public function create(Job $job)
+    {
+        return view('job_application.create', ['job' => $job]);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
