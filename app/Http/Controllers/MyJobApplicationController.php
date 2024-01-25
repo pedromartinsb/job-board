6ebd2bdfa00d2c6f60dd5bc41043ee70414dd26a
Pedro Martins Barbosa Campos <engpedrocampos@gmail.com>
2024-01-25 12:43:51 -0300

    feat: Cancelling Applications

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class MyJobApplicationController extends Controller
@@ -23,8 +24,13 @@ class MyJobApplicationController extends Controller
         );
     }
 
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+        $myJobApplication->delete();
+
+        return redirect()->back()->with(
+            'success',
+            'Job application removed.'
+        );
     }
 }
