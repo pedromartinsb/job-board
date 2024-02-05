1b646c10647a0d4fab07304dccb2a33591c0225b
Pedro Martins Barbosa Campos <engpedrocampos@gmail.com>
2024-02-05 19:05:06 -0300

    feat: adding jobs logic

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class MyJobController extends Controller
 {
@@ -28,7 +29,18 @@ class MyJobController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'location' => 'required|string|max:255',
+            'salary'=> 'required|numeric|min:5000',
+            'description'=> 'required|string',
+            'experience'=> 'required|in:' . implode(',', Job::$experience),
+            'category'=> 'required|in:' . implode(',', Job::$category),
+        ]);
+        auth()->user()->employer->jobs()->create($validatedData);
+
+        return redirect()->route('my-jobs.index')
+            ->with('success','Job created successfully.');
     }
 
     /**
